21b81c81136e876f853d5623b3a9cd75e1a65adc
moomoomoo123 <106563335+moomoomoo123@users.noreply.github.com>
2022-06-29 22:21:40 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+console.log (first_name);
+console.log (last_name);
+
+var merge = first_name.concat(last_name);
+console.log (merge);
+
+var a = 49;
+var b = 17;
+
+var add = a + b ;
+console.log(add);
+
+var subtract = b - a ;
+console.log(subtract);
